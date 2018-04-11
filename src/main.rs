e9ff2e8f504c0b42709c4d37188488df91f398da
pSox <pSox-genver(a)st8l.com>
2018-04-11 17:57:08 +0100

    Checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,45 @@
-#![cfg_attr(feature = "clippy", feature(plugin))]
-#![cfg_attr(feature = "clippy", plugin(clippy))]
-
 #[macro_use]
 extern crate clap;
+extern crate config;
 
 use clap::{App, AppSettings, Arg, SubCommand};
+use config::{Config, Value};
 
 fn main() {
+    let mut settings = Config::new().set_default("test", "value");
+
     let matches = App::new(crate_name!())
         .about(crate_description!())
         .version(crate_version!())
         .subcommand(
-            SubCommand::with_name("version")
-                .about("modify the version part of the string")
+            SubCommand::with_name("inc")
+                .about("increment the ...")
                 .setting(AppSettings::SubcommandRequiredElseHelp)
                 .subcommand(
-                    SubCommand::with_name("inc")
-                        .about("roll the version of a component")
+                    SubCommand::with_name("version")
+                        .about("version part")
+                        .setting(AppSettings::ArgRequiredElseHelp)
+                        .arg(
+                            Arg::with_name("part")
+                                .help("which component should be incremented")
+                                .index(1)
+                                .required(true),
+                        ),
+                )
+                .subcommand(
+                    SubCommand::with_name("prefix")
+                        .about("prefix part")
+                        .setting(AppSettings::ArgRequiredElseHelp)
+                        .arg(
+                            Arg::with_name("part")
+                                .help("which component should be incremented")
+                                .index(1)
+                                .required(true),
+                        ),
+                )
+                .subcommand(
+                    SubCommand::with_name("build")
+                        .about("build part")
                         .setting(AppSettings::ArgRequiredElseHelp)
                         .arg(
                             Arg::with_name("part")
@@ -29,5 +52,6 @@ fn main() {
         .get_matches();
 
     println!("{:#?}", matches);
+    println!("{:#?}", settings);
     // Continued program logic goes here...
 }
