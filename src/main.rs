bef7c67c0a68bc4a15bc5f9fcc5cf52ba67a8d8a
pSox <pSox-genver(a)st8l.com>
2018-04-12 23:00:42 +0100

    Added SemVer Default config

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,82 @@
 #[macro_use]
 extern crate clap;
-extern crate config;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_yaml;
 
 use clap::{App, AppSettings, Arg, SubCommand};
-use config::{Config, Value};
-use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MinMax<T> {
+    min: T, // minimum number of parts
+    max: T, // maximum number of parts
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum EntryType {
+    None,                         // nothing
+    RegEx(String),                // regular expresion
+    Exact(String),                // exact match
+    Front(String),                // front part of item
+    Back(String),                 // end part of item
+    PartType(u8, Box<EntryType>), // specific for this part
+    PartName(u8, String),         // the name of this part
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Section {
+    name: String,           // name of the section
+    parts: MinMax<u8>,      // minimum and maximum
+    flag: Option<char>,     // what signifies the start of this section
+    custom: Vec<EntryType>, // customize this section
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct GenVer {
+    name: String,           // Name of this configuration
+    separator: char,        // this separates parts in all sections
+    sections: Vec<Section>, // Describe each section
+}
 
 fn main() {
-    let default = HashMap.
-    let mut settings = Config::new();
-    settings
-        .set_default("k1", Value::from())
-        .unwrap();
+    let defaults = GenVer {
+        name: String::from("Semantic Versioning"),
+        separator: '.',
+        sections: vec![
+            Section {
+                name: String::from("version"),
+                parts: MinMax { min: 3, max: 3 },
+                flag: None,
+                custom: vec![
+                    EntryType::PartType(1, Box::new(EntryType::RegEx(String::from("v?\\d+")))),
+                    EntryType::PartName(1, String::from("major")),
+                    EntryType::PartType(2, Box::new(EntryType::RegEx(String::from("\\d+")))),
+                    EntryType::PartName(2, String::from("minor")),
+                    EntryType::PartType(3, Box::new(EntryType::RegEx(String::from("\\d+")))),
+                    EntryType::PartName(3, String::from("patch")),
+                ],
+            },
+            Section {
+                name: String::from("prefix"),
+                parts: MinMax { min: 1, max: 1 },
+                flag: Some('-'),
+                custom: vec![
+                    EntryType::PartType(
+                        1,
+                        Box::new(EntryType::RegEx(String::from("(alpha|beta|rc)?\\d+"))),
+                    ),
+                ],
+            },
+            Section {
+                name: String::from("build"),
+                parts: MinMax { min: 1, max: 1 },
+                flag: Some('+'),
+                custom: vec![EntryType::Front(String::from("v"))],
+            },
+        ],
+    };
+    let defaults_yaml = serde_yaml::to_string(&defaults).unwrap();
+    //let mut settings = Config::new();
 
     let matches = App::new(crate_name!())
         .about(crate_description!())
@@ -57,6 +122,7 @@ fn main() {
         .get_matches();
 
     println!("{:#?}", matches);
-    println!("{:#?}", settings);
+    println!("{:#?}", defaults);
+    println!("{}", defaults_yaml);
     // Continued program logic goes here...
 }
